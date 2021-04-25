3b1c96670be98fa171c549b8995c47885936598a
Cocosmos <mipam.guillot@crea-inseec.com>
2021-04-25 20:10:24 +0200

    calcul with data

--- index.php
+++ index.php
@@ -76,8 +76,25 @@ if(isset($_REQUEST["email"])){
         if (FALSE === $info) {
             echo $err;
         } else {
-            echo 'La boite aux lettres contient '.$info->Nmsgs.' message(s)';
+            $mails=$info->Nmsgs;
+            $mailsrate=$mails * 0.006;
+            //Un mail = 0.005kg 
+            //données pour un kilos
+            echo '<p>Votre boite aux lettres contient '.$mails.' message(s)<br> 
+            Taux de pollution actuelle en moyenne : '.$mailsrate.' kg de CO2 par an</p>
+            <p>La consommation de '. 12*$mailsrate.' kWh d’électricité (en France)<br> 
+            '. 2*$mailsrate.' jours d’éclairage avec 1 ampoule à incandescence (et '. 12*$mailsrate.' jours avec une 1 ampoule Basse Consommation)<br> 
+            La fabrication de '. 100*$mailsrate.' feuilles de papier de 80g<br> 
+            La fabrication de '. 1.5*$mailsrate.'kg de sucre<br> 
+            '. 9*$mailsrate.' km parcourus avec une voiture essence d’étiquette B<br> 
+            '. 6*$mailsrate.' km parcourus avec une voiture essence d’étiquette E</p>';
+
+            //https://www.faguo-store.com/fr/lunivers-faguo/lunivers-faguo/mission-engagements/mesurer/1kg-equivalent-de-co2/
+
+
+
         }
+
     }
 }
 ?>
