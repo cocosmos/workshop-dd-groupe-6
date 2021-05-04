e67c5d7819fd476b14ec9976c85adf9c1891a0ba
cocosmos <creamipam@gmail.com>
2021-05-04 12:38:01 +0200

    header

--- header.php
+++ header.php
@@ -4,25 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <link rel="icon" type="image/jpeg" src="media/logo/favicon.jpeg" />
     <title>E-Cobox</title>
 </head>
-    <Header>
+<Header>
         <div class="logo">
-        <img src="media/logo/logo_V5-8.png">
+        <img src="media/logo/logo_V5-8.png" height="136px">
         </div>
 
         <div class="menu">
             <ul>
-                <li>accueil</li>
-                <li>solutions</li>
+                <li><a href="index.php">accueil</a></li>
+                <li><a href="solution.php">solutions</a></li>
             </ul>
         </div>
-
-        <div class="profile">
-            <p><?php if(isset($_SESSION["name"])){
-            echo($_COOKIE["name"]);}?></p>
-            <a href="profile.php">voir le profil</a>
-        </div>
+        
     </Header>
